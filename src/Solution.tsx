b353ff0f92ac9b89c8ebb7ed58c7ba5e32c2a63e
Madhuranga Senadheera <madhuranga.senadeera@softlogiclife.lk>
2022-11-30 12:52:39 +0400

    fixed issue in typescript edit and remove indentation error

--- src/Solution.tsx
+++ src/Solution.tsx
@@ -1,35 +1,35 @@
 import React, { ReactElement, useState, useEffect} from 'react'; 
  
 type MenuItem = {
-    title: string;
-    subItems?: Array<string>;
-  };
+  title: string;
+  subItems?: Array<string>;
+};
   
-  type MenuConfig = Array<MenuItem>;
+type MenuConfig = Array<MenuItem>;
    
-  function Solution({ menuConfig }: { menuConfig: MenuConfig }): ReactElement {
-  const [buttonStatus, setButtonStatus] = useState({}); 
+function Solution({ menuConfig }: { menuConfig: MenuConfig }): ReactElement {
+  const [buttonStatus, setButtonStatus] = useState<any>({}); 
 
   const toggleBtnValue =  ( firstLevelTitle:string ) => {
-		// @ts-ignore 
+		 
     const btnCurrentStatus:Boolean = buttonStatus[firstLevelTitle] 
     setButtonStatus({...buttonStatus, [firstLevelTitle]:!btnCurrentStatus})
   }
   
   function loadButton(firstLevelTitle:string, subItems:Array<string>) {
     if(subItems!==undefined) {
-        return(
-            <button 
-                className={firstLevelTitle} 
-                onClick={(e)=>{toggleBtnValue(firstLevelTitle)}} 
-							// @ts-ignore
-            > {buttonStatus[firstLevelTitle]?'Hide':'Show'}</button>
-		)
+      return(
+        <button 
+            className={firstLevelTitle} 
+            onClick={(e)=>{toggleBtnValue(firstLevelTitle)}} 
+          
+        > {buttonStatus[firstLevelTitle]?'Hide':'Show'}</button>
+		  )
     }
   }
 
   const loadSubItems = (firstLevelTitle:string, subItems?:Array<string>) => {
-		// @ts-ignore
+		
 		if(buttonStatus[firstLevelTitle] && subItems) { 
 			// console.log(`msg_ loadSubItems`,firstLevelTitle, buttonStatus[firstLevelTitle], subItems);
      	return (
@@ -45,24 +45,21 @@ type MenuItem = {
 
 	const mainMenuLoad = ({title, subItems}:{title:string, subItems?:Array<string>}, index:number) => {  
     return(
-        <li key={index.toString()} className="{title}">
-            {title}
-            {loadButton(title, subItems || [])}
-            {loadSubItems(title, subItems)}
-        </li>
+      <li key={index.toString()} className="{title}">
+        {title}
+        {loadButton(title, subItems || [])}
+        {loadSubItems(title, subItems)}
+      </li>
     ) 
   }
 
   const loadDiv = (menuConfig:MenuConfig) => {
     return(
-			<ul> 
-				{ menuConfig.map( (mainRecord, index) => mainMenuLoad(mainRecord, index) ) } 
-			</ul>
+			<ul> { menuConfig.map( (mainRecord, index) => mainMenuLoad(mainRecord, index) ) } </ul>
 		) 
   }
   
-  return <div className="menu-wrapper" >{ loadDiv(menuConfig) }</div>;
-  return <div className="menu-wrapper" > <h1>Solution</h1></div>;
+  return <div className="menu-wrapper" >{ loadDiv(menuConfig) }</div>; 
 }
 
 export default Solution; 
